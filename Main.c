bb591bee34749b0a00a12586b533b171b7798406
PsychoBurns <matheusclaudino2006@hotmail.com>
2021-05-14 21:04:43 -0300

    Adicionado a função esvaziar

---
+++ Main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fila.h"
+
+int main(void) {
+
+  enfileirar(1);
+  enfileirar(2);
+  enfileirar(3);
+  
+  exibir();
+
+  printf("Desenfileirando:\n\n");
+  printf("%i\n", desenfileirar());
+  printf("%i\n", desenfileirar());
+  printf("%i\n", desenfileirar());
+
+  printf("\n\n");
+
+  exibir();
+
+  esvaziar();
+
+  return 0;
+}
